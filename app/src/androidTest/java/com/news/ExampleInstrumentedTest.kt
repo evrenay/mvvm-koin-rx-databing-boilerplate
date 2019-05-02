dc8de83927fa137098dc3528ca495df28e38f4bc
evrenay <evren.ay@azimech.com>
2019-05-02 14:50:50 +0300

    -son güncel

--- app/src/androidTest/java/com/news/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/news/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.cikmissorular
+package com.news
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
--- app/src/test/java/com/news/ExampleUnitTest.kt
+++ app/src/test/java/com/news/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.cikmissorular
+package com.news
 
 import org.junit.Test
 
